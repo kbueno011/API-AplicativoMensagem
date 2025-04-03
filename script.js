0ffe37106fe0f4022b171c1e8dc4deebec056d15
KAIKE BUENO <24122303@CFP127ALUNO.LOCAL>
2025-04-03 17:21:26 -0300

    Parte funcional concluida

---
+++ script.js
@@ -0,0 +1,58 @@
+'use strict'
+
+async function listaDeContatos(numero) {
+    const url = `https://giovanna-whatsapp.onrender.com/v1/whatsapp/contatos/${numero}`
+    const response = await fetch(url)
+    const dados = await response.json()
+    return dados.dados_contato
+}
+
+async function listaDeConversas(numero, contato) {
+    const url = `https://giovanna-whatsapp.onrender.com/v1/whatsapp/conversas?numero=${numero}&contato=${encodeURIComponent(contato)}`
+    const response = await fetch(url)
+    const dados = await response.json()
+    return dados.conversas[0].conversas
+}
+
+async function preencherConversas(contato) {
+    const numero = document.getElementById('numero').value
+    const mensagens = await listaDeConversas(numero, contato)
+    const chatBox = document.getElementById('chat')
+
+    chatBox.replaceChildren()
+
+    mensagens.forEach(mensagem => {
+        const msgDiv = document.createElement('div')
+        msgDiv.textContent = `${mensagem.sender} ${mensagem.content} (${mensagem.time})`
+        msgDiv.classList.add(mensagem.sender === "me" ? "mensagem-enviada" : "mensagem-recebida")
+        chatBox.appendChild(msgDiv)
+    })
+}
+
+async function preencherContatos() {
+    const numero = document.getElementById('numero').value
+    const contatos = await listaDeContatos(numero)
+    const galeria = document.getElementById('galeria')
+
+    galeria.replaceChildren()
+
+    contatos.forEach(contato => {
+        const contatoDiv = document.createElement('div')
+        contatoDiv.classList.add('contato-item')
+        contatoDiv.addEventListener('click', () => preencherConversas(contato.name))
+
+        const img = document.createElement('img')
+        img.src = './img/user.png' // Todos os contatos usarão essa imagem
+        img.alt = contato.name
+
+        const nome = document.createElement('span')
+        nome.textContent = contato.name
+        nome.classList.add('contato-nome')
+
+        contatoDiv.appendChild(img)
+        contatoDiv.appendChild(nome)
+        galeria.appendChild(contatoDiv)
+    })
+}
+
+document.getElementById('pesquisar').addEventListener('click', preencherContatos)
